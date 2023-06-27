2049a66841d591c47ae88e23839604868740c5fa
stratm <kelvin.kipkemboi@strathmore.edu>
2023-06-27 16:51:58 +0300

    2

--- app/Http/Middleware/checkAdmin.php
+++ app/Http/Middleware/checkAdmin.php
@@ -2,30 +2,25 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated
+class checkAdmin
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @param  string|null  ...$guards
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle(Request $request, Closure $next, $guard = null)
     {
-        if ($guard == "admin" && Auth::guard($guard)->check()) {
-            return redirect('/admin');
+        if (Auth::guard($guard)->check() && $request-> user()->isAdmin) {
+            return $next($request);
         }
-  
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
-        }
-        return $next($request);
-        }
+        session()->flash('unauthorized', 'You are not authorized to access the page '.$request->path());
+        return redirect('../home');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,6 +11,8 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    public $timestamps = false;
 
     /**
      * The attributes that are mass assignable.
@@ -21,7 +23,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'address',
     ];
+
+    protected $guard = 'isAdmin';
 
     /**
      * The attributes that should be hidden for serialization.
@@ -41,4 +46,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function orders() {
+        return $this->hasMany(Order::class); 
+    }
 }
